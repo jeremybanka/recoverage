7767055e2d475986d6f7d1db0cdc7b213076137f
Jeremy Banka <hello@jeremybanka.com>
2025-10-23 10:27:10 -0700

    🐛 handle apparent edge case where a range in the branch's locations can be {}

--- recoverage.cloud/__tests__/v8.test.ts
+++ recoverage.cloud/__tests__/v8.test.ts
@@ -1,9 +1,9 @@
-import { createCoverageMap } from "istanbul-lib-coverage"
+import { ArkErrors } from "arktype"
 
 import { v8CoverageMapType } from "../src/reporter.ts"
 import { v8ReportFixture } from "./report-fixture.ts"
 
 test(`validate schema`, () => {
 	const a = v8CoverageMapType(v8ReportFixture as any)
-	console.log(a)
+	expect(a).not.toBeInstanceOf(ArkErrors)
 })
